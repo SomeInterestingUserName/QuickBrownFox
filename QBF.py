8577ea4cb96012a35329630bd2982e297c3f6c25
Jiawei Chen <jchen2120212@gmail.com>
2016-10-09 22:46:39 -0400

    Add files via upload
    Initial Commit

---
+++ QBF.py
@@ -0,0 +1,22 @@
+#QuBrFx--"Quick Brown Fox Test"--Searches a string and returns whether or not it contains all letters of the alphabet.
+#
+#Revision History
+#   -1.0 Initial Release
+
+def QuBrFx(string_in, charset="abcdefghijklmnopqrstuvwxyz"):
+    letters = list(charset)
+    lettercount = 0
+    string_in = string_in.lower()
+    for items in letters:
+        if items.lower() in string_in:
+            lettercount += 1
+        else:
+            break
+    return lettercount == len(letters)
+
+if __name__ == '__main__': #Executes a test case if program is directly run. Just to test.
+    import time
+    start = time.clock()
+    result = QuBrFx("The quick brown fox jumped over the lazy dog.")
+    print(time.clock() - start)
+    print(result)
